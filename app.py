9e01f4ab5e59d23b485dd75f11a05e215f215924
Dhruv Upadhyay <40516305+IntManear@users.noreply.github.com>
2020-07-23 01:29:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ sms = africastalking.SMS
 @app.route('/', methods=['POST', 'GET'])
 def ussd_callback():
     global response
-    session_id = request.values.get("SessionID", None)
+    session_id = request.values.get("sessionID", None)
     service_code = request.values.get("serviceCode", None)
     phone_number = request.values.get("phoneNumber", None)
     text = request.values.get("text","default")
